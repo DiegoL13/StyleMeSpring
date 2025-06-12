d463363b343d871ad30ffe5c4338607af9078d44
Diego Luiz <166581174+DiegoL13@users.noreply.github.com>
2025-06-12 11:05:42 -0300

    Update StylemeApplication.java

--- styleme/styleme/styleme/src/main/java/com/styleme/projeto/StylemeApplication.java
+++ styleme/styleme/styleme/src/main/java/com/styleme/projeto/StylemeApplication.java
@@ -1,18 +1,7 @@
 package com.styleme.projeto;
 
-import com.styleme.projeto.entity.Cliente;
-import com.styleme.projeto.repository.UserRepository;
-import com.styleme.projeto.service.UserService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
-
-import java.util.Optional;
-import java.util.UUID;
 
 @SpringBootApplication
 public class StylemeApplication {
